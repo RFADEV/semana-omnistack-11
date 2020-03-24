83bf16c9c492fb40f7bf6e91b16bae9ea13670a7
rfadev <filgueiras.rfa@gmail.com>
2020-03-24 16:49:57 -0300

    Back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar uma informação no backend
+ * POST: Criar uma informação no backend
+ * PUT: Laterar uma informação no backend
+ * DELETE: Deletar uma informação no backend
+ */
+
+
+
+ /**
+  * Tipos de parâmentros
+  * 
+  * Query Params : Parâmetros nomeados enviados na rota após o simbolo de '?' (Filtros, Paginação)
+  * Route Params: Parâmetros utilizados para identificar recursos
+  * Request Body: Corpo da requisição utilizado para criar ou alterar recursos
+  * 
+  */
+
+/**
+ * MongoDB
+ * CouchDB
+ * etc
+ */
+
+ /**
+  * Driver: Select * from users
+  * Query Builder: table('users').select('*').where();
+  */
+
